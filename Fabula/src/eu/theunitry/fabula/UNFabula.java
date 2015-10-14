bd954b67f6ed7f36b57dc33b7a66ac07b2d565c5
MaartenBode <maartenbodepc@gmail.com>
2015-10-14 14:13:20 +0200

    Made title static

--- Fabula/src/eu/theunitry/fabula/UNFabula.java
+++ Fabula/src/eu/theunitry/fabula/UNFabula.java
@@ -2,7 +2,9 @@ package eu.theunitry.fabula;
 
 public class UNFabula {
 
+    private static String TITLE = "Fabula";
+
     public static void main(String[] args) {
-        System.out.println("Fabula");
+        System.out.println(this.TITLE);
     }
 }
