e287af948b4c8510e57a18bfe4446f20eaa9b520
MaartenBode <maartenbodepc@gmail.com>
2015-10-14 14:22:38 +0200

    Revert static title

--- Fabula/src/eu/theunitry/fabula/UNFabula.java
+++ Fabula/src/eu/theunitry/fabula/UNFabula.java
@@ -2,9 +2,8 @@ package eu.theunitry.fabula;
 
 public class UNFabula {
 
-    private static String TITLE = "Fabula";
 
     public static void main(String[] args) {
-        System.out.println(this.TITLE);
+        System.out.println("Fabula");
     }
 }
