e3f64f6c6da289d11c7562d285b7844c5c26d6cf
Jelmer Portegijs <japortegijs@gmail.com>
2015-11-04 13:24:00 +0100

    Started creating UNBlock class.

---
+++ Fabula/src/eu/theunitry/fabula/graphics/UNBlock.java
@@ -0,0 +1,23 @@
+package eu.theunitry.fabula.graphics;
+
+public class UNBlock extends UNGraphicsObject {
+
+    private boolean solid;
+    public boolean attribute;
+
+    public boolean getSolid() {
+        return solid;
+    }
+
+    public void setSolid(boolean solid) {
+
+    }
+
+    public boolean getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(boolean attribute) {
+
+    }
+}
