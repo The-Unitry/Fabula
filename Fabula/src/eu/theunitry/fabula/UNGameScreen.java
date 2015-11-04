d12a37e1fbe89685e581055ade927ef8d3bf3fd1
Allan de Wit <allandewit@live.nl>
2015-11-04 13:08:32 +0100

    Added test for JPanel

--- Fabula/src/eu/theunitry/fabula/UNGameScreen.java
+++ Fabula/src/eu/theunitry/fabula/UNGameScreen.java
@@ -1,16 +1,30 @@
 package eu.theunitry.fabula;
 
 
+import eu.theunitry.fabula.graphics.UNPanel;
 import eu.theunitry.fabula.graphics.UNWindow;
 import eu.theunitry.fabula.objects.UNObject;
+import eu.theunitry.fabula.objects.UNTimer;
+
+import javax.swing.*;
 
 public class UNGameScreen extends UNObject
 {
 
     private final UNWindow window;
+    private UNTimer gameLoop;
+    private UNPanel panel;
 
     public UNGameScreen() {
-        window = new UNWindow("Fabula", 768, 512);
+        this.window = new UNWindow("Fabula", 768, 512);
+        this.gameLoop = new UNTimer(this, 5);
+        this.panel = new UNPanel();
+
+        this.window.addPanel(this.panel);
+    }
+
+    public JPanel getPanel() {
+        return this.panel;
     }
 
 }
