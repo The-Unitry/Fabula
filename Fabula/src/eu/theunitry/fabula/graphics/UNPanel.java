96e186d0e416dfc61d5eeb6f8ad2b0c65a234b8a
Allan de Wit <allandewit@live.nl>
2015-11-05 10:53:24 +0100

    UNPanel almost done, TODO: HUD

--- Fabula/src/eu/theunitry/fabula/graphics/UNPanel.java
+++ Fabula/src/eu/theunitry/fabula/graphics/UNPanel.java
@@ -1,45 +1,54 @@
 package eu.theunitry.fabula.graphics;
 
+import eu.theunitry.fabula.UNGameScreen;
 import eu.theunitry.fabula.objects.UNObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class UNPanel extends JPanel
 {
 
+    private Image backgroundImage;
+    private ArrayList<UNGraphicsObject> objects;
+    private ArrayList hud;
+
     public UNPanel()
     {
-
+        objects = new ArrayList<UNGraphicsObject>();
+        hud = new ArrayList<UNGraphicsObject>();
     }
 
     @Override
     public void paintComponent(Graphics g)
     {
-        layerBackground();
-        layerBackgroundEffects();
-        layerBackObjects();
-        layerFrontObject();
+        g.drawImage(getBackgroundImage(), 0, 0, this);
+        layerObjects(g);
+        layerHUD();
     }
 
-    public void layerBackground()
+    public void setBackgroundImage(Image backgroundImage)
     {
-
+        this.backgroundImage = backgroundImage;
     }
 
-    public void layerBackgroundEffects()
+    public Image getBackgroundImage()
     {
-
+        return this.backgroundImage;
     }
 
-    public void layerBackObjects()
+    public void layerObjects(Graphics g)
     {
-
+        for (UNGraphicsObject object : objects)
+        {
+            g.drawImage(object.getImage(), object.getX(), object.getY(), this);
+        }
     }
 
-    public void layerFrontObject()
+    public void layerHUD()
     {
-
+        //TODO: HUDObjects?
     }
 
 }
