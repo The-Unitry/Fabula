2982fe40e7e55fb5e2ffa84506f193a4834482af
JBronkhorst <j.bronkhorst98@gmail.com>
2015-11-05 11:37:03 +0100

    Added Calculate class

--- Fabula/src/eu/theunitry/fabula/calculate/UNCalculate.java
+++ Fabula/src/eu/theunitry/fabula/calculate/UNCalculate.java
@@ -3,5 +3,38 @@ package eu.theunitry.fabula.calculate;
 /**
  * Created by Jeroen on 5-11-2015.
  */
-public class UNCalculate {
+
+public class UNCalculate{
+
+    private int g1, g2, result;
+
+    public int sum(){
+
+        result = g1+g2;
+        return result;
+
+    }
+
+    public int substract(){
+
+        result = g1 - g2;
+        return result;
+
+    }
+
+    public int multiply(){
+
+        result = g1*g2;
+        return result;
+
+    }
+
+    public int divide(){
+
+        result = g1/g2;
+        return result;
+
+    }
+
 }
+
