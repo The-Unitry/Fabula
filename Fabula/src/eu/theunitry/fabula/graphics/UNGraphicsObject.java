0ee8877aa6039c8639f536ce6858a967e76337ac
Jelmer Portegijs <japortegijs@gmail.com>
2015-11-05 09:14:20 +0100

    Created UNGraphicsObject, added getters and setters.

--- Fabula/src/eu/theunitry/fabula/graphics/UNGraphicsObject.java
+++ Fabula/src/eu/theunitry/fabula/graphics/UNGraphicsObject.java
@@ -3,6 +3,64 @@ package eu.theunitry.fabula.graphics;
 
 import eu.theunitry.fabula.objects.UNObject;
 
+import java.awt.*;
+
 public abstract class UNGraphicsObject extends UNObject
 {
+
+    private int x;
+    private int y;
+    private Image image;
+    private boolean clickable;
+    private Rectangle hitbox;
+
+    public int getX()
+    {
+        return this.x;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public int getY()
+    {
+        return this.y;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public Image getImage()
+    {
+        return this.image;
+    }
+
+    public void setImage(Image image)
+    {
+        this.image = image;
+    }
+
+    public boolean getClickable()
+    {
+        return this.clickable;
+    }
+
+    public void setClickable(boolean clickable)
+    {
+        this.clickable = clickable;
+    }
+
+    public Rectangle getHitbox()
+    {
+        return this.hitbox;
+    }
+
+    public void setHitbox(Rectangle hitbox)
+    {
+        this.hitbox = hitbox;
+    }
 }
