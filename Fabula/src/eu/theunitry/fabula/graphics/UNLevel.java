d63568b924c7f5f605ddbc21b099c251de0b907c
wesleymeyerwm <wesleymeyerwm@hotmail.com>
2015-11-11 15:36:06 +0100

    Created UNLevel.

---
+++ Fabula/src/eu/theunitry/fabula/graphics/UNLevel.java
@@ -0,0 +1,22 @@
+package eu.theunitry.fabula.graphics;
+
+/**
+ * Created by Wesley on 11/11/2015.
+ */
+public class UNLevel extends UNPanel {
+    public boolean playerWin = false;
+
+    public void win()
+    {
+        if (playerWin = true);
+        {
+            /** hud.helperText.setText = "Goed gedaan!"; */
+            playerWin = false;
+        }
+    }
+}
+
+
+
+
+
