72dd51905b943574ef3f6ae6d6e02675802aa0f6
Maarten Bode <maartenbodepc@gmail.com>
2015-11-12 09:12:07 +0100

    Remove styling from start button

--- Fabula/src/eu/theunitry/fabula/launcher/UNLauncher.java
+++ Fabula/src/eu/theunitry/fabula/launcher/UNLauncher.java
@@ -1,6 +1,7 @@
 package eu.theunitry.fabula.launcher;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class UNLauncher extends JPanel
@@ -13,6 +14,7 @@ public class UNLauncher extends JPanel
     {
         this.setBackground(new Color(255,82,44));
         this.setLayout(new BorderLayout());
+        this.setBorder(new EmptyBorder(10, 10, 10, 10));
 
         label = new JLabel("Fabula");
         label.setForeground(Color.white);
@@ -21,6 +23,15 @@ public class UNLauncher extends JPanel
 
         startBtn = new JButton("Start");
         startBtn.setPreferredSize(new Dimension(200,100));
+
+        /* Remove default styling */
+        startBtn.setFocusPainted(false);
+        startBtn.setBorderPainted(false);
+
+        startBtn.setBackground(Color.white);
+        startBtn.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
+        startBtn.setForeground(Color.white);
+
 
         this.add(label, BorderLayout.CENTER);
         this.add(startBtn, BorderLayout.SOUTH);
