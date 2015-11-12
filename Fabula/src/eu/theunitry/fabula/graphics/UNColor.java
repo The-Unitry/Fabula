b4f111f8cd849121738c5ccc18ce6131c3277947
Maarten Bode <maartenbodepc@gmail.com>
2015-11-12 09:47:42 +0100

    Renamed primary color, it's an constant now.

--- Fabula/src/eu/theunitry/fabula/graphics/UNColor.java
+++ Fabula/src/eu/theunitry/fabula/graphics/UNColor.java
@@ -4,18 +4,19 @@ import java.awt.*;
 
 public class UNColor
 {
-    private Color primaryColor;
+    private Color PRIMARY_COLOR;
 
     public UNColor()
     {
         this.setPrimaryColor(new Color(255,82,44));
     }
 
+
     public Color getPrimaryColor() {
-        return primaryColor;
+        return PRIMARY_COLOR;
     }
 
     public void setPrimaryColor(Color primaryColor) {
-        this.primaryColor = primaryColor;
+        PRIMARY_COLOR = primaryColor;
     }
 }
