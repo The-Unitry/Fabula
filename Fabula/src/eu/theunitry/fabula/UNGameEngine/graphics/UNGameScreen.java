e8866383c9ed2b33ad7c6076c713d1bbf626322b
JBronkhorst <j.bronkhorst98@gmail.com>
2015-12-04 13:30:14 +0100

    Added Sprites Level11 (Supermarket level)

--- Fabula/src/eu/theunitry/fabula/UNGameEngine/graphics/UNGameScreen.java
+++ Fabula/src/eu/theunitry/fabula/UNGameEngine/graphics/UNGameScreen.java
@@ -155,6 +155,7 @@ public class UNGameScreen extends UNObject
             backgrounds.add(0, ImageIO.read(new File("res/backgrounds/underwater.png")));
             backgrounds.add(1, ImageIO.read(new File("res/backgrounds/moon.png")));
             backgrounds.add(2, ImageIO.read(new File("res/backgrounds/space.png")));
+            backgrounds.add(3, ImageIO.read(new File("res/backgrounds/supermarket.png")));
 
             //SPRITE PRELOAD
             //TUILTJE IDLE
@@ -222,7 +223,13 @@ public class UNGameScreen extends UNObject
             //LEVEL8
             sprites.add(54, ImageIO.read(new File("res/sprites/klomp.png")));
             sprites.add(55, ImageIO.read(new File("res/sprites/pickaxe.png")));
-
+            //LEVEL11
+            sprites.add(56, ImageIO.read(new File("res/sprites/appel.png")));
+            sprites.add(57, ImageIO.read(new File("res/sprites/peer.png")));
+            sprites.add(58, ImageIO.read(new File("res/sprites/druif.png")));
+            sprites.add(59, ImageIO.read(new File("res/sprites/sinaasappel.png")));
+            sprites.add(60, ImageIO.read(new File("res/sprites/banaan.png")));
+            sprites.add(61, ImageIO.read(new File("res/sprites/kassa.png")));
         }
         catch (IOException e)
         {
