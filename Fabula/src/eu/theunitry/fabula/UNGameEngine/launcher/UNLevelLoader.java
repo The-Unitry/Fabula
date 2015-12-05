bdfe6bbc66969ea60707c712d7a4a5d3c7a67882
Maarten Bode <maartenbodepc@gmail.com>
2015-12-05 14:50:27 +0100

    Fix indentation issue

--- Fabula/src/eu/theunitry/fabula/UNGameEngine/launcher/UNLevelLoader.java
+++ Fabula/src/eu/theunitry/fabula/UNGameEngine/launcher/UNLevelLoader.java
@@ -6,7 +6,6 @@ import eu.theunitry.fabula.UNGameEngine.graphics.UNView;
 
 public class UNLevelLoader
 {
-    
     private int currentLevel = 1;      // The current level, this is temporary because the progress will be saved locally.
 
     public UNLevelLoader(UNGameScreen gameScreen)
