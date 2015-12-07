b2a9254404fab9f3d90278ae02e16cea9eebdfd9
Maarten Bode <maartenbodepc@gmail.com>
2015-12-07 11:49:21 +0100

    Change comment style of resources

--- Fabula/src/eu/theunitry/fabula/UNGameEngine/launcher/UNResourceLoader.java
+++ Fabula/src/eu/theunitry/fabula/UNGameEngine/launcher/UNResourceLoader.java
@@ -10,6 +10,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * This class is used for loading all resources. It should be replaced later with
+ * a more dynamic way of loading, since we are repeating ourselves a lot here.
+ */
 public class UNResourceLoader
 {
     public ArrayList<Music> music;
@@ -57,35 +61,47 @@ public class UNResourceLoader
     {
         try
         {
-            //BACKGROUND PRELOAD
+            /**
+             * Background
+             */
             backgrounds.add(0, ImageIO.read(new File("res/backgrounds/underwater.png")));
             backgrounds.add(1, ImageIO.read(new File("res/backgrounds/moon.png")));
             backgrounds.add(2, ImageIO.read(new File("res/backgrounds/space.png")));
             backgrounds.add(3, ImageIO.read(new File("res/backgrounds/supermarket.png")));
 
-            //SPRITE PRELOAD
-            //TUILTJE IDLE
+            /**
+             * Tuiltje - Idle
+             */
             sprites.add(0, ImageIO.read(new File("res/animations/tuiltje/idle/idle0.png")));
             sprites.add(1, ImageIO.read(new File("res/animations/tuiltje/idle/idle1.png")));
             sprites.add(2, ImageIO.read(new File("res/animations/tuiltje/idle/idle2.png")));
             sprites.add(3, ImageIO.read(new File("res/animations/tuiltje/idle/idle3.png")));
             sprites.add(4, ImageIO.read(new File("res/animations/tuiltje/idle/idle4.png")));
             sprites.add(5, ImageIO.read(new File("res/animations/tuiltje/idle/idle5.png")));
-            //TUILTJE FLAPPING
+
+            /**
+             * Tuiltje - Flapping
+             */
             sprites.add(6, ImageIO.read(new File("res/animations/tuiltje/flapping/flapping0.png")));
             sprites.add(7, ImageIO.read(new File("res/animations/tuiltje/flapping/flapping1.png")));
             sprites.add(8, ImageIO.read(new File("res/animations/tuiltje/flapping/flapping2.png")));
             sprites.add(9, ImageIO.read(new File("res/animations/tuiltje/flapping/flapping3.png")));
             sprites.add(10, ImageIO.read(new File("res/animations/tuiltje/flapping/flapping4.png")));
             sprites.add(11, ImageIO.read(new File("res/animations/tuiltje/flapping/flapping5.png")));
-            //TUILTJE SAD
+
+            /**
+             * Tuiltje - Sad
+             */
             sprites.add(12, ImageIO.read(new File("res/animations/tuiltje/sad/sad0.png")));
             sprites.add(13, ImageIO.read(new File("res/animations/tuiltje/sad/sad1.png")));
             sprites.add(14, ImageIO.read(new File("res/animations/tuiltje/sad/sad2.png")));
             sprites.add(15, ImageIO.read(new File("res/animations/tuiltje/sad/sad3.png")));
             sprites.add(16, ImageIO.read(new File("res/animations/tuiltje/sad/sad4.png")));
             sprites.add(17, ImageIO.read(new File("res/animations/tuiltje/sad/sad5.png")));
-            //TUILTJE HAPPY
+
+            /**
+             * Tuiltje - Happy
+             */
             sprites.add(18, ImageIO.read(new File("res/animations/tuiltje/happy/happy0.png")));
             sprites.add(19, ImageIO.read(new File("res/animations/tuiltje/happy/happy1.png")));
             sprites.add(20, ImageIO.read(new File("res/animations/tuiltje/happy/happy2.png")));
@@ -96,7 +112,10 @@ public class UNResourceLoader
             sprites.add(25, ImageIO.read(new File("res/animations/tuiltje/happy/happy7.png")));
             sprites.add(26, ImageIO.read(new File("res/animations/tuiltje/happy/happy8.png")));
             sprites.add(27, ImageIO.read(new File("res/animations/tuiltje/happy/happy9.png")));
-            //TUILTJE QUESTIONING
+
+            /**
+             * Tuiltje - Questioning
+             */
             sprites.add(28, ImageIO.read(new File("res/animations/tuiltje/questioning/questioning0.png")));
             sprites.add(29, ImageIO.read(new File("res/animations/tuiltje/questioning/questioning1.png")));
             sprites.add(30, ImageIO.read(new File("res/animations/tuiltje/questioning/questioning2.png")));
@@ -107,11 +126,17 @@ public class UNResourceLoader
             sprites.add(35, ImageIO.read(new File("res/animations/tuiltje/questioning/questioning7.png")));
             sprites.add(36, ImageIO.read(new File("res/animations/tuiltje/questioning/questioning8.png")));
             sprites.add(37, ImageIO.read(new File("res/animations/tuiltje/questioning/questioning9.png")));
-            //LEVEL0
+
+            /**
+             * Level 0
+             */
             sprites.add(38, ImageIO.read(new File("res/sprites/apple.png")));
             sprites.add(39, ImageIO.read(new File("res/sprites/basket.png")));
             sprites.add(40, ImageIO.read(new File("res/sprites/snowball.png")));
-            //LEVEL3
+
+            /**
+             * Level 3
+             */
             sprites.add(41, ImageIO.read(new File("res/sprites/rocket0.png")));
             sprites.add(42, ImageIO.read(new File("res/sprites/rocket1.png")));
             sprites.add(43, ImageIO.read(new File("res/sprites/rocket2.png")));
@@ -122,14 +147,23 @@ public class UNResourceLoader
             sprites.add(48, ImageIO.read(new File("res/sprites/ufoGreen.png")));
             sprites.add(49, ImageIO.read(new File("res/sprites/ufoPink.png")));
             sprites.add(50, ImageIO.read(new File("res/sprites/snowball.png")));
-            //LEVEL7
+
+            /**
+             * Level 7
+             */
             sprites.add(51, ImageIO.read(new File("res/sprites/kist_dicht.png")));
             sprites.add(52, ImageIO.read(new File("res/sprites/kist_open.png")));
             sprites.add(53, ImageIO.read(new File("res/sprites/muntje.png")));
-            //LEVEL8
+
+            /**
+             * Level 8
+             */
             sprites.add(54, ImageIO.read(new File("res/sprites/klomp.png")));
             sprites.add(55, ImageIO.read(new File("res/sprites/pickaxe.png")));
-            //LEVEL11
+
+            /**
+             * Level 11
+             */
             sprites.add(56, ImageIO.read(new File("res/sprites/appel.png")));
             sprites.add(57, ImageIO.read(new File("res/sprites/peer.png")));
             sprites.add(58, ImageIO.read(new File("res/sprites/druif.png")));
