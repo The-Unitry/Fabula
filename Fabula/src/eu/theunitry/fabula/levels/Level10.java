a392e81411733f1fc81dcdfaf0f288f243464328
wesleymeyerwm <wesleymeyerwm@hotmail.com>
2015-12-10 00:22:45 +0100

    Preparing Level10 for finalization.

--- Fabula/src/eu/theunitry/fabula/levels/Level10.java
+++ Fabula/src/eu/theunitry/fabula/levels/Level10.java
@@ -20,9 +20,11 @@ import java.util.Random;
 public class Level10 extends UNGraphicsLevel
 {
     private Timer timer;
-    private UNGraphicsObject snail;
+    private UNGraphicsObject snail1;
+    private UNGraphicsObject snail2;
+    private UNGraphicsObject snail3;
     private ArrayList<UNGraphicsObject> acorn;
-    private UNGraphicsObject selecter;
+    private UNGraphicsObject pointer;
     private JButton button;
     private boolean winning;
     private int need, touch, g1, g2;
@@ -38,21 +40,29 @@ public class Level10 extends UNGraphicsLevel
     {
         super(gameScreen, hudEnabled);
 
-        this.g1 = 1+ new Random().nextInt(5);
-        this.g2 = 1+ new Random().nextInt(6);
+        this.g1 = 1+ new Random().nextInt(4);
+        this.g2 = 1+ new Random().nextInt(4);
         this.need = g1 * g2;
 
-        this.setQuestion("hoeveel groepen van " + g1 + " eikeltjes vallen uit de boom");
-        this.addHelp("Jammer! Je moet " + need + " gewichtjes op de snail hebben");
-        this.addHelp("Helaas! Er moeten " + need + " gewichtjes op de snail staan");
-        this.addHelp("net niet goed, weet je zeker dat er " + need + " op de snail staan?");
-        this.setHelp("Klik op het juiste getal om te antwoorden");
+        if (g1 == 1)
+        {
+            this.setQuestion("hoeveel groepen van " + g1 + " eikeltje vallen uit de boom");
+        } else {
+            this.setQuestion("hoeveel groepen van " + g1 + " eikeltjes vallen uit de boom");
+        }
+        
+        this.addHelp("Jammer! Je moet kijken hoevel groepen van " + g1 + " eikeltjes op de grond liggen");
+        this.addHelp("Helaas! gebruik je wel de tafel van " + g1 + " ?");
+        this.addHelp("net niet goed, weet je zeker dat je goed hebt getelt?");
+        this.setHelp("Sleep het grote eikeltje naar de slak met het juiste antwoord.");
         this.setBackgroundImage(gameScreen.unResourceLoader.backgrounds.get("forest-background"));
 
         this.winning = false;
         this.lastHelp = getHelp();
-        this.selecter = new UNGraphicsObject(gameScreen.getWindow().getFrame(), 342, 285, gameScreen.unResourceLoader.sprites.get("2:10:1"), true, 32, 32);
-        this.snail = new UNGraphicsObject(gameScreen.getWindow().getFrame(), 342, 285, gameScreen.unResourceLoader.sprites.get("2:10:2"), true, 32, 32);
+        this.pointer = new UNGraphicsObject(gameScreen.getWindow().getFrame(), 342, 285, gameScreen.unResourceLoader.sprites.get("2:10:1"), true, 32, 32);
+        this.snail1 = new UNGraphicsObject(gameScreen.getWindow().getFrame(), 557, 200, gameScreen.unResourceLoader.sprites.get("2:10:2"), false, 60, 60);
+        this.snail2 = new UNGraphicsObject(gameScreen.getWindow().getFrame(), 557, 270, gameScreen.unResourceLoader.sprites.get("2:10:2"), false, 60, 60);
+        this.snail3 = new UNGraphicsObject(gameScreen.getWindow().getFrame(), 557, 340, gameScreen.unResourceLoader.sprites.get("2:10:2"), false, 60, 60);
         this.acorn = new ArrayList<UNGraphicsObject>();
         this.color = new UNColor();
 
@@ -68,8 +78,10 @@ public class Level10 extends UNGraphicsLevel
         this.button.setFont(new Font("Minecraftia", Font.PLAIN, 15));
         this.button.setForeground(Color.white);
         this.button.setOpaque(true);
-        addObject(selecter);
-        addObject(snail);
+        addObject(pointer);
+        addObject(snail1);
+        addObject(snail2);
+        addObject(snail3);
 
         /**
          * Reset Default Styling
@@ -94,7 +106,7 @@ public class Level10 extends UNGraphicsLevel
                 if (isHelperDoneTalking()) {
                     if (winning) {
                         getHelper().setState(3);
-                        setHelp("Goed gedaan, de diamant is van ons");
+                        setHelp("Goed gedaan, jij kent de tafels goed!");
                         for (UNGraphicsObject acorns : acorn) {
                             acorns.setClickable(false);
                         }
@@ -111,12 +123,12 @@ public class Level10 extends UNGraphicsLevel
                             getHelper().setState(4);
                             if (touch < need) {
                                 setHelp("Jammer, er moest" + ((need - touch == 1) ? "" : "en") + " nog " + (need - touch) +
-                                        " gewichtjes" + ((need - touch == 1) ? "" : "s")  + " bij. Want " + touch + " plus " +
+                                        " groep(en)" + ((need - touch == 1) ? "" : "s")  + " bij. Want " + touch + " keer " +
                                         (need - touch)  + " is " + need
                                 );
                             } else {
                                 setHelp("Jammer, er moest" + ((touch - need == 1) ? "" : "en") + " " + (touch - need) +
-                                        " gewicht" + ((touch - need == 1) ? "" : "s")  + " af. Want " + touch + " min " +
+                                        " groep(en)" + ((touch - need == 1) ? "" : "s")  + " af. Want " + touch + " keer " +
                                         (touch - need)  + " is " + need
                                 );
                             }
@@ -133,13 +145,11 @@ public class Level10 extends UNGraphicsLevel
             addObject(acorns);
         }
 
-            addObject(snail);
-
 
         timer = new Timer(1, e -> {
             touch = 0;
             for (UNGraphicsObject acorns : acorn) {
-                if (snail.getHitbox().intersects(acorns.getHitbox())) {
+                if (snail1.getHitbox().intersects(acorns.getHitbox())) {
                     touch++;
                 }
             }
@@ -147,7 +157,7 @@ public class Level10 extends UNGraphicsLevel
 
             for(UNGraphicsObject acorns : acorn)
             {
-                if(!acorns.getMouseHold() && !snail.getHitbox(true).intersects(acorns.getHitbox())) {
+                if(!acorns.getMouseHold() && !snail2.getHitbox(true).intersects(acorns.getHitbox())) {
                     acorns.setY(acorns.getY() + 1);
                 }
             }
