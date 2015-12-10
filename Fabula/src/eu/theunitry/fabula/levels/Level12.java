bba71ea940325bbe2bd36edf46972bfea603a429
JBronkhorst <j.bronkhorst98@gmail.com>
2015-12-10 08:52:50 +0100

    Updated Level 12, moved cage to a better place

--- Fabula/src/eu/theunitry/fabula/levels/Level12.java
+++ Fabula/src/eu/theunitry/fabula/levels/Level12.java
@@ -54,7 +54,7 @@ public class Level12 extends UNGraphicsLevel
 
         this.color = new UNColor();
 
-        this.cage = new UNGraphicsObject(gameScreen.getWindow().getFrame(), 40, 260, gameScreen.unResourceLoader.sprites.get("2:12:1"), false, 128, 128);
+        this.cage = new UNGraphicsObject(gameScreen.getWindow().getFrame(), 35, 260, gameScreen.unResourceLoader.sprites.get("2:12:1"), false, 128, 128);
 
         for (int i = 0; i < 5; i++){
             monkeys_white.add(new UNGraphicsObject(gameScreen.getWindow().getFrame(), 150 +
